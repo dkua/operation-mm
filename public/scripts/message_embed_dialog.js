6ff9410c4ac7d33ece868fa3d4271aad02765894
Jaydon Soo <jaydonsky@gmail.com>
2024-12-01 00:36:30 +0800

    Clean up message_embed_dialog.js
    - Enclose in block and use `const` lambdas instead of `function` to
    avoid polluting global scope
    - Change attaching of event handlers to not create closures for each
    video placeholder
    - Fix formatting of JSDoc comments
    - Remove some random empty lines

--- public/scripts/message_embed_dialog.js
+++ public/scripts/message_embed_dialog.js
@@ -1,32 +1,39 @@
-/** @type {HTMLDialogElement} */
-const dialog = document.getElementById("video-dialog");
+{
+    /** @type {HTMLDialogElement} */
+    const dialog = document.getElementById("video-dialog");
 
-/**
-  * @param {string} video_id
-  *
-  */
-function open_video_dialog(video_id) {
-    const video_iframe = dialog.querySelector("iframe");
-    video_iframe.src = `https://www.youtube-nocookie.com/embed/${video_id}?autoplay=1`;
-    dialog.showModal();
-}
-
-dialog.addEventListener("pointerdown", (e) => {
-    if (e.target === dialog) {
-        window.addEventListener("pointerup", (e) => {
-            if (e.target === dialog) {
-
-                dialog.close();
-            }
-        }, { once: true });
+    /**
+     * @param {string} video_id
+     */
+    const open_video_dialog = (video_id) => {
+        const video_iframe = dialog.querySelector("iframe");
+        video_iframe.src = `https://www.youtube-nocookie.com/embed/${video_id}?autoplay=1`;
+        dialog.showModal();
     }
-});
-dialog.addEventListener("close", () => {
-    const video_iframe = dialog.querySelector("iframe");
-    video_iframe.src = "";
-});
-dialog.querySelector(".dialog-close-button").addEventListener("click", () => dialog.close());
 
-for (const el of document.getElementsByClassName('video placeholder')) {
-    el.addEventListener('click', () => open_video_dialog(el.getAttribute('data-video_id')));
+    /**
+     * @type EventListener
+     */
+    const handle_video_click = (ev) => {
+        open_video_dialog(ev.currentTarget.getAttribute("data-video_id"));
+    }
+
+    dialog.addEventListener("pointerdown", (e) => {
+        if (e.target === dialog) {
+            window.addEventListener("pointerup", (e) => {
+                if (e.target === dialog) {
+                    dialog.close();
+                }
+            }, { once: true });
+        }
+    });
+    dialog.addEventListener("close", () => {
+        const video_iframe = dialog.querySelector("iframe");
+        video_iframe.src = "";
+    });
+    dialog.querySelector(".dialog-close-button").addEventListener("click", () => dialog.close());
+
+    for (const el of document.getElementsByClassName('video placeholder')) {
+        el.addEventListener('click', handle_video_click);
+    }
 }
