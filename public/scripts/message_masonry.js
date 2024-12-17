90e3c57d791354d446d4b4590aff1959fb0a0aab
Jaydon Soo <jaydonsky@gmail.com>
2024-12-17 13:28:20 +0800

    Add `load()` calls for messages fonts
    Aside from maybe only Chrome, simply adding preload links for the font
    files is insufficient to get the initial layout to render with the fonts
    since the FontFace still only loads when the font is first used, i.e.
    when the messages get added to the DOM.
    
    Also remove preload link for Noto Sans JP, because the file apparently
    varies based on platform.

--- public/scripts/message_masonry.js
+++ public/scripts/message_masonry.js
@@ -2,14 +2,24 @@
     const msg_container = document.getElementById("msg-container");
     /** @type {HTMLTemplateElement} */
     const messages = document.getElementById("messages-template");
-    // Normally template content is cloned before placing in the DOM, but in this case
-    // the contents are only needed once
-    msg_container.replaceChildren(messages.content);
-    twemoji.parse(msg_container);
-    const message_masonry = new Masonry(msg_container, {
-        itemSelector: ".msg",
-        gutter: 20,
-        fitWidth: true,
-        layoutInstant: true,
+    for (const font of document.fonts) {
+        // Have to use `includes` because browsers are inconsistent about having quotes in the family
+        // TODO: Possibly load other Noto font chunks when we have the finalised messages. Depending on page load performance.
+        if ((font.family.includes("Noto Sans JP") && font.unicodeRange.startsWith("U+0-FF"))
+            || font.family.includes("Edo SZ")) {
+            font.load();
+        }
+    }
+    document.fonts.ready.then(() => {
+        // Normally template content is cloned before placing in the DOM, but in this case
+        // the contents are only needed once
+        msg_container.replaceChildren(messages.content);
+        twemoji.parse(msg_container);
+        const message_masonry = new Masonry(msg_container, {
+            itemSelector: ".msg",
+            gutter: 20,
+            fitWidth: true,
+            layoutInstant: true,
+        });
     });
 }
