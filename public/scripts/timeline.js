5a393ac62a2a57469fd6fa6ebfb6f3dccb8a9cf8
dkua <david@kua.io>
2025-01-02 23:49:58 -0500

    Clean up Timeline related Javascript
    Makes use of `DocumentFragment` to collect all the visible Timeline
    Events before appending it to Timeline. This should reduce the number of
    redraws that occur when filtering the Timeline.
    
    Also change the `event-type` attribute to be `data-event-type`.

--- public/scripts/timeline.js
+++ public/scripts/timeline.js
@@ -3,23 +3,24 @@
     const timeline = document.getElementById("timeline");
     const num_visible_events = document.getElementById("num_visible");
     const timeline_events = Array.from(timeline.children);
-    const hidden_types = new Map();
+    const hidden_types = new Set();
 
     type_filter.addEventListener("change", function(el) {
         if (el.target.checked) {
             hidden_types.delete(el.target.value);
-            timeline_events.forEach((te) => {
-                if (hidden_types.has(te.getAttribute("event-type"))) {
-                    return;
-                } else {
-                    timeline.append(te);
+            const visible_events = new DocumentFragment();
+            for (const te of timeline_events) {
+                if (!hidden_types.has(te.getAttribute("data-event-type"))) {
+                    visible_events.append(te);
                 };
-            });
+            };
+            timeline.append(visible_events);
         } else {
             // Hide this event type
-            const events_of_type = timeline.querySelectorAll(`[event-type=${el.target.value}]`);
-            hidden_types.set(el.target.value, events_of_type.length);
-            events_of_type.forEach((e) => e.remove());
+            hidden_types.add(el.target.value);
+            for (const e of timeline.querySelectorAll(`[data-event-type=${el.target.value}]`)) {
+                e.remove();
+            };
         };
         num_visible_events.innerText = timeline.children.length;
     });
