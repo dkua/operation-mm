0794075cc0f3ac0ca5bc912a65ad93e36801c915
Jaydon Soo <jaydonsky@gmail.com>
2025-01-03 12:07:55 +0800

    Change to using the FontFaceSet.load API
    Instead of iterating through FontFaces

--- public/scripts/message_masonry.js
+++ public/scripts/message_masonry.js
@@ -2,14 +2,9 @@
     const msg_container = document.getElementById("msg-container");
     /** @type {HTMLTemplateElement} */
     const messages = document.getElementById("messages-template");
-    for (const font of document.fonts) {
-        // Have to use `includes` because browsers are inconsistent about having quotes in the family
-        // TODO: Possibly load other Noto font chunks when we have the finalised messages. Depending on page load performance.
-        if ((!font.family.includes("JP") && !font.family.includes("KR") && font.family.includes("Noto Sans"))
-            || font.family.includes("Edo SZ")) {
-            font.load();
-        }
-    }
+    // TODO: Possibly load other Noto font chunks when we have the finalised messages. Depending on page load performance.
+    document.fonts.load("1em Noto Sans");
+    document.fonts.load("1em Edo SZ");
     document.fonts.ready.then(() => {
         // Normally template content is cloned before placing in the DOM, but in this case
         // the contents are only needed once
