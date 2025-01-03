8b798b0d8ef37acf735e776c988f229c999a084f
Jaydon Soo <jaydonsky@gmail.com>
2025-01-03 17:16:35 +0800

    Add preloading for non-English Noto Sans fonts

--- public/scripts/message_masonry.js
+++ public/scripts/message_masonry.js
@@ -2,9 +2,16 @@
     const msg_container = document.getElementById("msg-container");
     /** @type {HTMLTemplateElement} */
     const messages = document.getElementById("messages-template");
-    // TODO: Possibly load other Noto font chunks when we have the finalised messages. Depending on page load performance.
     document.fonts.load("1em Noto Sans");
     document.fonts.load("1em Edo SZ");
+    const charset_sans_jp = messages.getAttribute("data-charset-sans-jp");
+    const charset_sans_kr = messages.getAttribute("data-charset-sans-kr");
+    if (charset_sans_jp) {
+        document.fonts.load("1em Noto Sans JP", charset_sans_jp);
+    }
+    if (charset_sans_kr) {
+        document.fonts.load("1em Noto Sans KR", charset_sans_kr);
+    }
     document.fonts.ready.then(() => {
         // Normally template content is cloned before placing in the DOM, but in this case
         // the contents are only needed once
